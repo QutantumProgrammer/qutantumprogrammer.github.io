f785be74e5af09764e83cb3d5e0443cfdcf06c4b
QutantumProgrammer <zcfxs@hotmail.com>
2017-03-20 03:36:31 +0800

    success

--- Rank/app/commonModule/app.js
+++ Rank/app/commonModule/app.js
@@ -1,5 +1,17 @@
 
 define(["angular", "jquery", "bootstrap"], function () {
         var app = angular.module("rankApp", []);
+        app.directive('onFinishRenderFilters', function ($timeout) {
+			return {
+				restrict: 'A',
+					link: function(scope, element, attr) {
+						if (scope.$last === true) {
+							$timeout(function() {
+							scope.$emit('ngRepeatFinished');
+							});
+						}
+					}
+				};
+			});
         return app;
 });
